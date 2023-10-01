56904b29d05c0b0f61689ff926d5858369ccaf81
Monu <monu.nigam.bit@gmail.com>
2023-10-01 12:39:06 +0530

    Removed extra space and reduced space complexity to O(1)

--- single.java
+++ single.java
@@ -6,15 +6,10 @@ class single{
     for(int i=0;i<15;i++){
       arr[i]=sc.nextInt();
     }
-    HashMap<Integer,Integer> hm = new HashMap<>();
+    int bit=0;
     for(int i:arr){
-      hm.put(i,hm.getOrDefault(i,0)+1);
-    }
-    for(int i:hm.keySet()){
-      if(hm.get(i)==1){
-        System.out.println("Single number = "+i);
-        break;
-      }
-    }
+      bit = (bit^i);
+  }
+	System.out.println("Single number = "+bit);
   }
 }
